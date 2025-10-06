eda4229cf2c745517d60adc97e98cb99f3ea8903
zDefcon <antholozadarequelme@gmail.com>
2025-10-06 02:00:09 -0500

    Initial commit - Proyecto Estacionamiento de Autos POO

---
+++ src/Aplicacion/Auto.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aplicacion;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Auto extends VehiculoPadre {
+    private int puertas;
+    private final double IVA = 0.15;
+    
+    public Auto(){
+        
+    }
+    
+    public Auto(int puertas){
+        this.puertas = puertas;
+    }
+    
+    public Auto(String marca, double precio, int puertas){
+        super(marca, precio);
+        this.puertas = puertas;
+    }
+    
+    public void setPuertas(int puertas){
+        this.puertas = puertas;
+    }
+    
+    public void setPrecio(double precio){
+        if(precio > 0){
+            alerta(precio);
+        this.precio = precio;
+        }
+        else{
+            alerta(precio);
+        }
+    }
+    
+    public void alerta(double precio){
+        if(precio < 0){
+        JOptionPane.showMessageDialog(null,"Precio Ingresado Invalido: " + precio, "ERROR", JOptionPane.ERROR_MESSAGE);
+        }else{   
+            JOptionPane.showMessageDialog(null,"Precio Ingresado: " + precio, "Info", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+    
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+}
---
+++ src/Aplicacion/VehiculoPadre.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aplicacion;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class VehiculoPadre {
+    protected String marca;
+    protected double precio;
+    
+    public VehiculoPadre(){
+        
+    }
+    
+    public VehiculoPadre(String marca, double precio){
+        this.marca = marca;
+        this.precio = precio;
+    }
+    
+    public void setPrecio(double precio){
+        this.precio = precio;
+    }
+    
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    
+    public String mostrarInfo(){
+        return "Marca; " + marca + "\n" + "Precio: " + precio + "\n";
+    }
+    
+}
